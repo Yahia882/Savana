6d31846488971108dfe9904154bf32025fcc7a1f
Yahia882 <yahia.abdo2002@gmail.com>
2024-11-27 23:37:05 +0200

    create super user

--- build.sh
+++ build.sh
@@ -10,3 +10,12 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py migrate
+
+echo "import os
+from django.contrib.auth.models import User
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@example.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'password123')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+" | python manage.py shell
