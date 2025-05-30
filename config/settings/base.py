57c2b7b0084770ebea76bf39ae5b09d2290097ec
Yahia882 <yahia.abdo2002@gmail.com>
2025-05-30 18:11:37 +0300

    minor change

--- config/settings/base.py
+++ config/settings/base.py
@@ -17,8 +17,8 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR,  "mediafiles")
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.path.join(BASE_DIR,  "mediafiles")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
